83a673883cbb1fa3adf2320e3cbb5aa26caee711
aleixnieto <aleixnieto@gmail.com>
2022-02-04 18:38:58 +0100

    Clean and english 4

--- 4.Cluster k-means.R
+++ 4.Cluster k-means.R
@@ -2,12 +2,8 @@
 # Title: k-means clustering
 # Author: Aleix Nieto
 # Date: 11/21 - 12/21
-# Description: Statistical study of a mice dataframe provided by IBAPS & IBEC
+# Description: K-means clustering + mean variable plot comparison between clusters
 ################################################################################
-
-#Cargar y utilizar función IPAK
-#ver vídeo https://www.youtube.com/watch?v=UjQz9SxG9rk
-#https://www.youtube.com/watch?v=7AFuL-1Q8eg&ab_channel=PabloVallejoMedina
 
 ipak <- function(pkg){
   new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
@@ -19,8 +15,23 @@ ipak <- function(pkg){
 packages <- c("tidyverse","cluster", "factoextra","NbClust","tidyr")
 ipak(packages)
 
-setwd("C:/Users/garys/Desktop/PRACTIQUES/ESTUDI ESTADÍSTIC RATOLINS/")
+setwd("C:/Users/garys/Desktop/PRACTIQUES/MICE STATISTICAL ANALYSIS/DATAFRAMES GENERATED/")
 dd<- read.table("datapreprocessed.csv", header=T, sep=",", fileEncoding = 'UTF-8-BOM');
+
+v<-list(
+  categoric=c('gender','diet','time','group'),
+  integer=c('final_auc','fasting_glucosa_final'),
+  continua=c('final_weight','weight_gain','LV_weight','LV_ratio','WAT_weight','WAT_ratio',
+             'insulin_0_final','insulin_15_final','homa_ir','homa_beta','liver_trigly'))
+
+for(i in v$continua) dd[,i]<-as.numeric(as.character(gsub(",",".",dd[,i],fixed=TRUE)))
+for(i in v$categoric) dd[[i]]<-as.factor(dd[[i]])
+for(i in v$integer) dd[[i]]<-as.integer(dd[[i]])
+
+sapply(dd,class)
+
+v$numeric<-c(v$integer,v$continua)
+
 dd.num <- dd[,5:17]
 
 # Normalize data
@@ -76,27 +87,27 @@ res3 <- hcut(dd.num, k = 3, stand = TRUE)
 fviz_dend(res3, rect = TRUE, cex = 0.5, show_labels = FALSE, k_colors = c("red","#2E9Fdd","green"))
 
 # Mean of each cluster for every variable
-dd.num <- dd[,5:17]
+dd.num <- dd[,v$numeric]
 dd.num %>%
   mutate(Cluster = k2$cluster) %>%
   group_by(Cluster) %>%
   summarise_all("mean")
 
 # Pass the clusters to my initial dd.num to work with them
-dd$clus<-as.factor(k2$cluster)
-dd
-
 dd.num <- scale(dd.num)
 dd.num<- as.data.frame(dd.num)
 dd.num$clus<-as.factor(k2$cluster)
 dd.num
 
 dd.num$clus<-factor(dd.num$clus)
-data_long <- gather(dd.num, caracteristica, valor, final_weight:liver_trigly, factor_key=TRUE)
+data_long <- gather(dd.num, caracteristica, value, v$numeric, factor_key=TRUE)
 data_long
 
-ggplot(data_long, aes(feature, y = value,group=clus, colour = clus)) +
+ggplot(data_long, aes(caracteristica, y = value, group=clus, colour = clus)) +
   stat_summary(fun = mean, geom="pointrange", size = 1)+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
   stat_summary(geom="line")
 geom_point(aes(shape=clus))
+
+
+
